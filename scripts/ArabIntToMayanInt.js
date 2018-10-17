fdd1ec743940ce1324ce1d99f81daa4e2f1945f1
wolfram74 <phaugen@gmail.com>
2018-10-17 10:32:15 -0600

    string method working

---
+++ scripts/ArabIntToMayanInt.js
@@ -0,0 +1,25 @@
+var ArabIntToMayanInt = (function(){
+  var API = {}
+  API.toString = function(number){
+    var power = 1
+    if(number===0){return 'o'}
+    var output = ''
+    while(number > 0){
+      var remainder = number%Math.pow(20, power)
+      var placeVal = remainder / Math.pow(20, power-1)
+      var ones = placeVal % 5
+      var fives = (placeVal-ones)/5
+      if(ones===0 && fives==0){
+        output = ',o' + output
+        power+=1
+        continue
+      }
+      output = ','+'d'.repeat(ones)+'b'.repeat(fives)+output
+      number -= remainder
+      power+=1
+    }
+
+    return output.slice(1)
+  }
+  return API
+})()
